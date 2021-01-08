6962652309dbd907f4cef2261c08a52b09f3b9c3
Martin <martinnovoar2@gmail.com>
2021-01-08 02:41:39 -0300

    Manage cars basic view

--- src/modules/car/controller/carController.js
+++ src/modules/car/controller/carController.js
@@ -9,8 +9,9 @@ module.exports = class CarController {
 
   configureRoutes(app) {
     app.get('/', this.index.bind(this));
-    app.get(`${this.ROUTE}/car`, this.list.bind(this));
+    app.get(`${this.ROUTE}/list`, this.list.bind(this));
     app.get(`${this.ROUTE}/delete/:id`, this.delete.bind(this));
+    app.get('/testadd', this.testadd.bind(this));
   }
 
   async index(req, res) {
@@ -21,9 +22,17 @@ module.exports = class CarController {
 
   async list(req, res) {
     const cars = await this.carService.getAll();
+    console.log(cars);
+
+    res.render(`${this.views}/list.njk`, { cars });
   }
 
   async delete(req, res) {
     this.carService.delete(req.params.id);
+  }
+
+  async testadd(req, res) {
+    const car = new Car(null, 'brand', 'model', 2000, 2, 'color', true, 5, true, 42, 'asd');
+    this.carService.carRepository.save(car);
   }
 };
