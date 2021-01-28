4128000d560921746e573717024d24558e0d3a33
Martin <martinnovoar2@gmail.com>
2021-01-28 18:42:49 -0300

    Migration updated for user model.

--- data/migrations/20210104193524-migration-skeleton.js
+++ data/migrations/20210104193524-migration-skeleton.js
@@ -1,11 +1,14 @@
 const CarModel = require('../../src/modules/car/model/carModel');
+const UserModel = require('../../src/modules/user/model/userModel');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     CarModel.setup(queryInterface.sequelize).sync({ forced: true });
+    UserModel.setup(queryInterface.sequelize).sync({ forced: true });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('cars');
+    await queryInterface.dropTable('users');
   },
 };
